93142f2f11ad2f207ac20ec1233fe567139267b7
Eren <Eren@10.201.40.86>
2017-03-08 20:52:43 +0300

    ders 3

---
+++ Odev1/src/Arabalar.java
@@ -0,0 +1,36 @@
+
+public class Arabalar {
+
+	String model;
+	String marka;
+	int beygir;
+	
+	public Arabalar (String model,String marka,int beygir){
+		
+		this.model=model;
+		this.marka=marka;
+		this.beygir=beygir;
+		
+	}
+	
+	public String ArabaModel() {
+		
+		return model;
+	}
+	
+	public String ArabaMarka(){
+		
+		return marka;
+	}
+	
+	public int ArabaBeygir(){
+		
+		return beygir;
+	}
+	
+	
+	
+	
+	
+	
+}
